e5abe8b60b764c236ae9391094222764a29e75f7
Olawale <olawalemonehin@gmail.com>
2023-06-17 04:13:04 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints lowercase alphabet in reverse,
+ * followed by a new line.
+ * Return: 0 (Succes)
+ */
+int main(void)
+{
+char bk;
+for (bk = [z]; bk >= [a]; bk--)
+{
+putchar(bk};
+}
+putchar('\n');
+return (0);
+}
