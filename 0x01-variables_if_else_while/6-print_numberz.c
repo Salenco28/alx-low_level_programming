31f9bd39b96b48c38aca0ed6bcf5e175a831efe8
Olawale <olawalemonehin@gmail.com>
2023-06-17 03:31:54 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints number from 0-9
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 45; i < 55; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
